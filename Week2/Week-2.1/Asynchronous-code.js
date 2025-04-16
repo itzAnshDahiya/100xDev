f27c9b9455a3262a497785f567d201e077fc37e0
itzAnshDahiya <anshdahiya00@gmail.com>
2025-04-16 19:17:25 +0530

    added new code

--- Week2/Week-2.1/Asynchronous-code.js
+++ Week2/Week-2.1/Asynchronous-code.js
@@ -7,7 +7,7 @@ function print(err,data){
     }
 }
 fs.readFile("aas.txt" , "utf-8" , print);
-console.log("Done!!");
+console.log("Done!");
 console.log(chalk.blue('I Am Always One Step Ahead'));
 console.log(chalk.blue('I Am Gona Rule This World'));
 console.log(chalk.blue('I Am Gona Become A Billionaire'));
