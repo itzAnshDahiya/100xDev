56dfad359ef47e511c831add7d9f63c67a44647a
itzAnshDahiya <anshdahiya00@gmail.com>
2025-04-24 16:56:59 +0530

    added new code

--- Week6/Week-6.1/index.js
+++ Week6/Week-6.1/index.js
@@ -2,13 +2,25 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+//[{username:"Ansh", password: 123 , token: "asdasdasadasd"}]
 
 const users = [];
+
+//should return a logn random string
 function generateToken(){
-    
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' , 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    let token = "";
+    for(let i = 0 ; i <32 ; i++){
+        //use a simple function here
+        token = token + options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
 }
 
-app.post("/singup", function(req,res){
+app.post("/signup", function(req,res){
     const username = req.body.username;
     const password = req.body.password;
 
@@ -22,8 +34,25 @@ app.post("/singup", function(req,res){
     })
 })
 
-app.post("/singup", function(req,res){
+app.post("/signin", function(req,res){
+      const username = req.body.username;
+      const password = req.body.password;
 
+     //maps and filters
+     let foundUsers = null;
+
+     for(let i = 0; i < users.length ; i++){
+        if(users[i].username == username && users[i].password == password){
+            foundUsers = users[i]
+        }
+     }
+     if(foundUsers){
+        const token = generateToken();
+foundUsers.token = token;
+        res.json({
+            message: token
+        })
+     }
 })
 
 app.listen(3000);
