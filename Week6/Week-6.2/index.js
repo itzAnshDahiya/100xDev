f768e5bb27a2322eb385798f5b0b9da482ad324e
itzAnshDahiya <anshdahiya00@gmail.com>
2025-04-26 20:56:44 +0530

    added new code

---
+++ Week6/Week-6.2/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "kirat123123";
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+function logger(req, res, next) {
+    console.log(req.method + " request came");
+    next();
+}
+
+// localhost:3000
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/signup", logger, function(req, res) {
+    const username = req.body.username
+    const password = req.body.password
+    users.push({
+        username: username,
+        password: password
+    })
+
+    // we should check if a user with this username already exists
+
+    res.json({
+        message: "You are signed in"
+    })
+})
+
+app.post("/signin", logger, function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+            foundUser = users[i]
+        }
+    }
+
+    if (!foundUser) {
+        res.json({
+            message: "Credentials incorrect"
+        })
+        return 
+    } else {
+        const token = jwt.sign({
+            username: users[i].username
+        }, JWT_SECRET);
+        res.header("jwt", token);
+
+        res.header("random", "harkirat");
+
