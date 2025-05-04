c114030fa8f12bed2b6260f68b7439a1925f53e9
itzAnshDahiya <anshdahiya00@gmail.com>
2025-05-04 23:45:43 +0530

    added some content

---
+++ Week7/Week-7.1/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.post("/signup", function(req , res){
+    
+});
+
+app.post("/signin", function(req , res){
+    
+});
+
+app.post("/todo", function(req , res){
+    
+});
+
+app.post("/todos", function(req , res){
+    
+});
+
+app.listen(3000);
