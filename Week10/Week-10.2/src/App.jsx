8bb22e7ab14cc79f2284ff11a3919da80b8c2212
itzAnshDahiya <anshdahiya00@gmail.com>
2025-05-20 19:13:18 +0530

    added new code

---
+++ Week10/Week-10.2/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return<div> 
+      <LightBulb />
+    </div>
+}
+
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true);
+  return<div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+    </div>
+}
+
+function BulbState({ bulbOn }) {
+  return <div>{bulbOn ? "Bulb On" : "Bulb Off"}</div>;
+}
+
+function ToggleBulbState({ bulbOn, setBulbOn }) {
+  function toggle() {
+    setBulbOn(!bulbOn);
+  }
+
+  return <div>
+      <button onClick={toggle}>Toggle The Bulb</button>
+    </div>
+}
+
+export default App;
