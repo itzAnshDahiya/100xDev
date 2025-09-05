0ae50767ddee6d097d36bee50ed788295ef10f0c
itzAnshDahiya <anshdahiya00@gmail.com>
2025-09-05 15:59:46 +0530

    added new code

--- Week4/Express/kidney.js
+++ Week4/Express/kidney.js
@@ -58,15 +58,10 @@ app.post("/", function (req, res) {
 
 // DELETE: remove all unhealthy kidneys
 app.delete("/", function(req, res){
-  const newKidneys = [];
-  for (let i = 0; i < users[0].kidneys.length; i++) {
-    if (users[0].kidneys[i].healthy) {
-      newKidneys.push({ healthy: true });
-    }
-  }
-  users[0].kidneys = newKidneys;
+  users[0].kidneys = users[0].kidneys.filter(kidney => kidney.healthy);
   res.json({ msg: "done" });
 });
+// ..
 
 // PUT: update a kidney's healthy status (by index)
 app.put("/:kidneyIndex", function (req, res) {
@@ -86,3 +81,65 @@ app.put("/:kidneyIndex", function (req, res) {
 app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
+
+// Here Is Same Code But With Different Functionality
+  // const express = require("express");
+  // const app = express();
+  // app.use(express.json());
+
+  // const users = [
+  //   {
+  //     name: "john",
+  //     kidneys: [{ healthy: false }],
+  //   },
+  // ];
+
+  // // GET all kidneys info
+  // app.get("/", (req, res) => {
+  //   const kidneys = users[0].kidneys;
+  //   const healthy = kidneys.filter(k => k.healthy).length;
+  //   const unhealthy = kidneys.length - healthy;
+  //   res.json({
+  //     kidneys,
+  //     healthyKidneys: healthy,
+  //     unhealthyKidneys: unhealthy,
+  //   });
+  // });
+
+  // // POST: add a new kidney
+  // app.post("/", (req, res) => {
+  //   const { healthy } = req.body;
+  //   if (typeof healthy !== "boolean") {
+  //     return res.status(400).json({ msg: "healthy must be boolean" });
+  //   }
+  //   users[0].kidneys.push({ healthy });
+  //   res.json({ msg: "Kidney added." });
+  // });
+
+  // // DELETE: remove all unhealthy kidneys
+  // app.delete("/", (req, res) => {
+  //   const before = users[0].kidneys.length;
+  //   users[0].kidneys = users[0].kidneys.filter(k => k.healthy);
+  //   const after = users[0].kidneys.length;
+  //   res.json({ msg: `Removed ${before - after} unhealthy kidneys.` });
+  // });
+
+  // // PUT: update a kidney's healthy status (by index)
+  // app.put("/:kidneyIndex", (req, res) => {
+  //   const idx = parseInt(req.params.kidneyIndex);
+  //   const { healthy } = req.body;
+  //   if (
+  //     isNaN(idx) ||
+  //     idx < 0 ||
+  //     idx >= users[0].kidneys.length ||
+  //     typeof healthy !== "boolean"
+  //   ) {
+  //     return res.status(400).json({ msg: "Invalid index or healthy value." });
+  //   }
+  //   users[0].kidneys[idx].healthy = healthy;
+  //   res.json({ msg: "Kidney updated." });
+  // });
+
+  // app.listen(3001, () => {
+  //   console.log("Server running on port 3001");
+  // });
