436754e81252ee6a3282202d9172e8bff0ba58dc
itzAnshDahiya <anshdahiya00@gmail.com>
2025-09-12 00:11:12 +0530

    successfully tested the code

--- Week7/Week-7.2/auth.js
+++ Week7/Week-7.2/auth.js
@@ -36,36 +36,61 @@
 
 //--------------Kirat code----------------
 
-// Import jwt modules
-const jwt = require("jsonwebtoken");
+// // Import jwt modules
+// const jwt = require("jsonwebtoken");
 
-// Create a JWT_SECRET variable for the secret key
+// // Create a JWT_SECRET variable for the secret key
+// const JWT_SECRET = "hellobacchomajaloclasska";
+
+// // Create an auth middleware function to authenticate the user
+// function auth(req, res, next) {
+//     // Get the token from the request headers
+//     const token = req.headers.authorization;
+
+//     // Verify the token using the jwt.verify() method
+//     const decodedData = jwt.verify(token, JWT_SECRET);
+
+//     // If the token is valid, set the userId in the request object and call the next middleware
+//     if (decodedData) {
+//         // Set the userId in the request object
+//         req.userId = decodedData.id;
+
+//         // Call the next middleware
+//         next();
+//     } else {
+//         // If the token is invalid, send an error message to the client
+//         res.status(403).json({
+//             message: "Incorrect Credentials!",
+//         });
+//     }
+// }
+
+// // Export the auth middleware function and JWT_SECRET for use in other files
+// module.exports = {
+//     auth,
+//     JWT_SECRET
+// }
+
+
+const jwt = require("jsonwebtoken");
 const JWT_SECRET = "hellobacchomajaloclasska";
 
-// Create an auth middleware function to authenticate the user
 function auth(req, res, next) {
-    // Get the token from the request headers
-    const token = req.headers.authorization;
-
-    // Verify the token using the jwt.verify() method
-    const decodedData = jwt.verify(token, JWT_SECRET);
-
-    // If the token is valid, set the userId in the request object and call the next middleware
-    if (decodedData) {
-        // Set the userId in the request object
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ message: "Token missing" });
+    }
+    // Support "Bearer <token>"
+    const token = authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : authHeader;
+    try {
+        const decodedData = jwt.verify(token, JWT_SECRET);
         req.userId = decodedData.id;
-
-        // Call the next middleware
         next();
-    } else {
-        // If the token is invalid, send an error message to the client
-        res.status(403).json({
-            message: "Incorrect Credentials!",
-        });
+    } catch (error) {
+        res.status(403).json({ message: "Incorrect Credentials!" });
     }
 }
 
-// Export the auth middleware function and JWT_SECRET for use in other files
 module.exports = {
     auth,
     JWT_SECRET
--- Week7/Week-7.2/index.js
+++ Week7/Week-7.2/index.js
@@ -143,158 +143,256 @@
 //--------------Kirat code----------------
 
 // Import the express, mongoose, jwt, bcrypt and zod modules
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+// const { z } = require("zod");
+
+// // Import the UserModel and TodoModel from the db.js file
+// const { UserModel, TodoModel } = require("./db");
+
+// // Import the auth middleware function and JWT_SECRET from the auth.js file
+// const { auth, JWT_SECRET } = require("./auth");
+
+// // Create an instance of the express module
+// const app = express();
+
+// // Parse the JSON data using the express.json() middleware
+// app.use(express.json());
+
+// // Connect to the MongoDB database using the mongoose.connect() method
+// mongoose.connect("mongodb+srv://Sinbad:1gKBgpkvVpggYpjA@sinbad.ypmtdr1.mongodb.net/Sinbad");
+
+// // Create a POST route for the signup endpoint
+// app.post("/signup", async function (req, res) {
+//     // Input Validation using Zod
+//     const requireBody = z.object({
+//         email: z.string().min(3).max(100).email(), // email is must be a string, min 3 characters, max 100 characters, and must be a valid email
+//         password: z.string().min(3).max(100), // password is must be a string, min 3 characters, max 100 characters
+//         name: z.string().min(3).max(100), // name is must be a string, min 3 characters, max 100 characters
+//     });
+
+//     // Parse the request body using the requireBody.safeParse() method to validate the data format
+//     const parseDataWithSuccess = requireBody.safeParse(req.body);
+
+//     // If the data format is incorrect, send an error message to the client
+//     if (!parseDataWithSuccess.success) {
+//         return res.json({
+//             message: "Incorrect data format",
+//             error: parseDataWithSuccess.error,
+//         });
+//     }
+
+//     // Get the email, password, and name from the request body
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const name = req.body.name;
+
+//     // Hash the password using the bcrypt.hash() method
+//     const hashedPassword = await bcrypt.hash(password, 5);
+//     // console.log(hashedPassword);
+
+//     // Error handling for creating a new user
+//     try {
+//         // Create a new user using the UserModel.create() method
+//         await UserModel.create({
+//             email: email,
+//             password: hashedPassword,
+//             name: name,
+//         });
+//     } catch (error) {
+//         // If the user already exists, send an error message to the client
+//         return res.json({
+//             message: "User already exists!",
+//         });
+//     }
+
+//     // Send a response to the client
+//     res.json({
+//         message: "You are signed up!",
+//     });
+// });
+
+// // Create a POST route for the signin endpoint
+// app.post("/signin", async function (req, res) {
+//     // Get the email and password from the request body
+//     const email = req.body.email;
+//     const password = req.body.password;
+
+//     // Find the user with the given email
+//     const user = await UserModel.findOne({
+//         email: email,
+//     });
+
+//     // If the user is not found, send an error message to the client
+//     if (!user) {
+//         return res.status(403).json({
+//             message: "Invalid Credentials!",
+//         });
+//     }
+
+//     // Compare the password with the hashed password using the bcrypt.compare() method
+//     const passwordMatch = await bcrypt.compare(password, user.password);
+
+//     // If the user password matches
+//     if (passwordMatch) {
+//         // Create a JWT token using the jwt.sign() method
+//         const token = jwt.sign(
+//             {
+//                 id: user._id.toString(),
+//             },
+//             JWT_SECRET
+//         );
+
+//         // Send the token to the client
+//         res.json({
+//             token: token,
+//             message: "You are signed in!",
+//         });
+//     } else {
+//         // If the user is not found, send an error message to the client
+//         res.status(403).json({
+//             message: "Invalid Credentials!",
+//         });
+//     }
+// });
+
+// // Create a POST route for the todo endpoint
+// app.post("/todo", auth, async function (req, res) {
+//     // Get the userId from the request object
+//     const userId = req.userId;
+
+//     // Get the title, and done from the request body
+//     const title = req.body.title;
+//     const done = req.body.done;
+
+//     // Create a new todo using the TodoModel.create() method
+//     await TodoModel.create({
+//         userId,
+//         title,
+//         done,
+//     });
+
+//     // Send a response to the client
+//     res.json({
+//         message: "Todo created",
+//     });
+// });
+
+// // Create a GET route for the todo endpoint
+// app.get("/todo", auth, async function (req, res) {
+//     // Get the userId from the request object
+//     const userId = req.userId;
+
+//     // Find all the todos with the given userId
+//     const todos = await TodoModel.find({
+//         userId,
+//     });
+
+//     // Send the todos to the client
+//     res.json({
+//         todos,
+//     });
+// });
+
+// // Start the server on port 3000
+// app.listen(3000);
+
+
+
+
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { z } = require("zod");
-
-// Import the UserModel and TodoModel from the db.js file
 const { UserModel, TodoModel } = require("./db");
-
-// Import the auth middleware function and JWT_SECRET from the auth.js file
 const { auth, JWT_SECRET } = require("./auth");
 
-// Create an instance of the express module
 const app = express();
-
-// Parse the JSON data using the express.json() middleware
 app.use(express.json());
 
-// Connect to the MongoDB database using the mongoose.connect() method
-mongoose.connect("mongodb+srv://100xdevs:WvaTca0509mb90YX@cluster0.ossjd.mongodb.net/todos-app-week-7-2");
-
-// Create a POST route for the signup endpoint
-app.post("/signup", async function (req, res) {
-    // Input Validation using Zod
+mongoose.connect("mongodb+srv://Sinbad:1gKBgpkvVpggYpjA@sinbad.ypmtdr1.mongodb.net/Sinbad");
+// Signup route
+app.post("/signup", async (req, res) => {
     const requireBody = z.object({
-        email: z.string().min(3).max(100).email(), // email is must be a string, min 3 characters, max 100 characters, and must be a valid email
-        password: z.string().min(3).max(100), // password is must be a string, min 3 characters, max 100 characters
-        name: z.string().min(3).max(100), // name is must be a string, min 3 characters, max 100 characters
+        email: z.string().min(3).max(100).email(),
+        password: z.string().min(3).max(100),
+        name: z.string().min(3).max(100),
     });
 
-    // Parse the request body using the requireBody.safeParse() method to validate the data format
-    const parseDataWithSuccess = requireBody.safeParse(req.body);
-
-    // If the data format is incorrect, send an error message to the client
-    if (!parseDataWithSuccess.success) {
-        return res.json({
+    const parsed = requireBody.safeParse(req.body);
+    if (!parsed.success) {
+        return res.status(400).json({
             message: "Incorrect data format",
-            error: parseDataWithSuccess.error,
+            error: parsed.error,
         });
     }
 
-    // Get the email, password, and name from the request body
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
+    const { email, password, name } = parsed.data;
 
-    // Hash the password using the bcrypt.hash() method
-    const hashedPassword = await bcrypt.hash(password, 5);
-    // console.log(hashedPassword);
-
-    // Error handling for creating a new user
     try {
-        // Create a new user using the UserModel.create() method
+        const hashedPassword = await bcrypt.hash(password, 5);
         await UserModel.create({
-            email: email,
+            email,
             password: hashedPassword,
-            name: name,
+            name,
         });
+        res.json({ message: "You are signed up!" });
     } catch (error) {
-        // If the user already exists, send an error message to the client
-        return res.json({
-            message: "User already exists!",
-        });
-    }
-
-    // Send a response to the client
-    res.json({
-        message: "You are signed up!",
-    });
-});
-
-// Create a POST route for the signin endpoint
-app.post("/signin", async function (req, res) {
-    // Get the email and password from the request body
-    const email = req.body.email;
-    const password = req.body.password;
-
-    // Find the user with the given email
-    const user = await UserModel.findOne({
-        email: email,
-    });
-
-    // If the user is not found, send an error message to the client
-    if (!user) {
-        return res.status(403).json({
-            message: "Invalid Credentials!",
-        });
-    }
-
-    // Compare the password with the hashed password using the bcrypt.compare() method
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    // If the user password matches
-    if (passwordMatch) {
-        // Create a JWT token using the jwt.sign() method
-        const token = jwt.sign(
-            {
-                id: user._id.toString(),
-            },
-            JWT_SECRET
-        );
-
-        // Send the token to the client
-        res.json({
-            token: token,
-            message: "You are signed in!",
-        });
-    } else {
-        // If the user is not found, send an error message to the client
-        res.status(403).json({
-            message: "Invalid Credentials!",
-        });
+        if (error.code === 11000) { // Duplicate key error
+            return res.status(409).json({ message: "User already exists!" });
+        }
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
-// Create a POST route for the todo endpoint
-app.post("/todo", auth, async function (req, res) {
-    // Get the userId from the request object
+// Signin route
+app.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(403).json({ message: "Invalid Credentials!" });
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (passwordMatch) {
+            const token = jwt.sign({ id: user._id.toString() }, JWT_SECRET);
+            res.json({ token, message: "You are signed in!" });
+        } else {
+            res.status(403).json({ message: "Invalid Credentials!" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// Create a todo
+app.post("/todos", auth, async (req, res) => {
     const userId = req.userId;
-
-    // Get the title, and done from the request body
-    const title = req.body.title;
-    const done = req.body.done;
-
-    // Create a new todo using the TodoModel.create() method
-    await TodoModel.create({
-        userId,
-        title,
-        done,
-    });
-
-    // Send a response to the client
-    res.json({
-        message: "Todo created",
-    });
+    const { title, done } = req.body;
+    try {
+        const todo = await TodoModel.create({ userId, title, done });
+        res.json({ message: "Todo created", todo });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 
-// Create a GET route for the todo endpoint
-app.get("/todo", auth, async function (req, res) {
-    // Get the userId from the request object
+// Get all todos for user
+app.get("/todos", auth, async (req, res) => {
     const userId = req.userId;
-
-    // Find all the todos with the given userId
-    const todos = await TodoModel.find({
-        userId,
-    });
-
-    // Send the todos to the client
-    res.json({
-        todos,
-    });
+    try {
+        const todos = await TodoModel.find({ userId });
+        res.json({ todos });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 
-// Start the server on port 3000
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
