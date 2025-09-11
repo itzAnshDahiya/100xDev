3fa4a7d142a97055ecd05e3d4e46b991def9ab11
itzAnshDahiya <anshdahiya00@gmail.com>
2025-09-11 22:39:12 +0530

    added new code

--- Week7/Week-7.1/db.js
+++ Week7/Week-7.1/db.js
@@ -1,27 +1,70 @@
-const mongoose = require("mongoose");  // Mongoose ko import kar rahe hain, jo MongoDB ke saath interact karta hai
-const Schema = mongoose.Schema;  // Mongoose ke Schema object ko extract kar rahe hain
-const ObjectId = mongoose.ObjectId;  // MongoDB ka ObjectId use kar rahe hain, jo references ko handle karta hai
+// const mongoose = require("mongoose");  // Mongoose ko import kar rahe hain, jo MongoDB ke saath interact karta hai
+// const Schema = mongoose.Schema;  // Mongoose ke Schema object ko extract kar rahe hain
+// const ObjectId = mongoose.ObjectId;  // MongoDB ka ObjectId use kar rahe hain, jo references ko handle karta hai
 
-// User Schema define kar rahe hain
+// // User Schema define kar rahe hain
+// const User = new Schema({
+//     email: String,   // User ka email
+//     password: String,  // User ka password
+//     name: String  // User ka name
+// });
+
+// // Todo Schema define kar rahe hain
+// const Todo = new Schema({
+//     title: String,  // Todo ka title
+//     done: Boolean,  // Kya todo complete ho gaya ya nahi (true/false)
+//     userId: ObjectId  // User ka reference (jo todo create karega)
+// });
+
+// // Mongoose model banane ke liye, jo MongoDB collection se interact karega
+// const UserModel = mongoose.model('users', User);  // "users" collection ke liye model
+// const TodoModel = mongoose.model('todos', Todo);  // "todos" collection ke liye model
+
+// // Models ko export kar rahe hain taaki baaki code mein use kiya ja sake
+// module.exports = {
+//     UserModel,  // UserModel ko export kar rahe hain
+//     TodoModel  // TodoModel ko export kar rahe hain
+// };
+
+
+
+//--------------Kirat code----------------
+
+// Import mongoose to interact with MongoDB
+const mongoose = require("mongoose");
+
+/*
+const User = new mongoose.Schema({
+    email: String,
+    password: String,
+    name: String,
+});
+*/
+
+// Use Schema and ObjectId from mongoose for creating models
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+// Define the User schema with fields for email, password, and name
 const User = new Schema({
-    email: String,   // User ka email
-    password: String,  // User ka password
-    name: String  // User ka name
+    email: { type: String, unique: true }, // Make email unique to avoid duplicate entries
+    password: String,
+    name: String,
 });
 
-// Todo Schema define kar rahe hain
+// Define the Todo schema with fields for title, done, and userId
 const Todo = new Schema({
-    title: String,  // Todo ka title
-    done: Boolean,  // Kya todo complete ho gaya ya nahi (true/false)
-    userId: ObjectId  // User ka reference (jo todo create karega)
+    title: String,
+    done: Boolean,
+    userId: ObjectId,
 });
 
-// Mongoose model banane ke liye, jo MongoDB collection se interact karega
-const UserModel = mongoose.model('users', User);  // "users" collection ke liye model
-const TodoModel = mongoose.model('todos', Todo);  // "todos" collection ke liye model
+// Create Mongoose models for users and todos collections using the User and Todo schemas
+const UserModel = mongoose.model("users", User);
+const TodoModel = mongoose.model("todos", Todo);
 
-// Models ko export kar rahe hain taaki baaki code mein use kiya ja sake
+// Export the User and Todo models for use in other files
 module.exports = {
-    UserModel,  // UserModel ko export kar rahe hain
-    TodoModel  // TodoModel ko export kar rahe hain
+    UserModel,
+    TodoModel,
 };
--- Week7/Week-7.1/index.js
+++ Week7/Week-7.1/index.js
@@ -1,108 +1,257 @@
-const express = require("express"); // Express module ko import kar rahe hain
-const { UserModel, TodoModel } = require("./db"); // User aur Todo models ko import kar rahe hain
-const jwt = require("jsonwebtoken"); // JWT module ko import kar rahe hain
-const JWT_SECRET = "asdasd@!23"; // JWT ke liye secret key
+// const express = require("express"); // Express module ko import kar rahe hain
+// const { UserModel, TodoModel } = require("./db"); // User aur Todo models ko import kar rahe hain
+// const jwt = require("jsonwebtoken"); // JWT module ko import kar rahe hain
+// const JWT_SECRET = "asdasd@!23"; // JWT ke liye secret key
 
-const app = express(); // Express app banate hain
-app.use(express.json()); // JSON body parsing ke liye middleware use kar rahe hain
+// const app = express(); // Express app banate hain
+// app.use(express.json()); // JSON body parsing ke liye middleware use kar rahe hain
 
-// Signup route: Naye user ko sign up karne ke liye
-app.post("/signup", async function(req, res) {
-    const { email, password, name } = req.body; // Email, password aur name ko request body se nikaal rahe hain
+// // Signup route: Naye user ko sign up karne ke liye
+// app.post("/signup", async function(req, res) {
+//     const { email, password, name } = req.body; // Email, password aur name ko request body se nikaal rahe hain
 
-    // UserModel ke through naye user ko database mein insert kar rahe hain
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
+//     // UserModel ke through naye user ko database mein insert kar rahe hain
+//     await UserModel.create({
+//         email: email,
+//         password: password,
+//         name: name
+//     });
 
-    // Response mein success message bhej rahe hain
+//     // Response mein success message bhej rahe hain
+//     res.json({
+//         message: "You are logged In"
+//     });
+// });
+
+// // Signin route: User ko sign in karne ke liye
+// app.post("/signin", async function(req, res) {
+//     const { email, password } = req.body; // Email aur password ko request body se nikaal rahe hain
+
+//     // UserModel se user ko database mein search kar rahe hain
+//     const user = await UserModel.findOne({
+//         email: email,
+//         password: password
+//     });
+
+//     console.log(user); // User ko console pe log kar rahe hain (debugging ke liye)
+
+//     if (user) {
+//         // Agar user milta hai, toh JWT token generate kar rahe hain
+//         const token = jwt.sign({
+//             id: user._id.toString() // User ka ID token mein embed kar rahe hain
+//         }, JWT_SECRET); // Secret key ke saath token ko sign kar rahe hain
+
+//         // Token ko response mein bhej rahe hain
+//         res.json({
+//             token: token
+//         });
+//     } else {
+//         // Agar user nahi milta, toh 403 error bhej rahe hain
+//         res.status(403).json({
+//             message: "Incorrect Credentials"
+//         });
+//     }
+// });
+
+// // Todo create karne ka route (protected route)
+// app.post("/todo", auth, function(req, res) {
+//     const userId = req.userId; // Auth middleware se userId ko access kar rahe hain
+//     const title = req.body.title; // Todo ka title request body se le rahe hain
+
+//     // TodoModel mein naya todo create kar rahe hain
+//     TodoModel.create({
+//         title: title,
+//         userId: userId
+//     });
+
+//     // Response mein userId bhej rahe hain
+//     res.json({
+//         userId: userId
+//     });
+// });
+
+// // Todos ko fetch karne ka route (protected route)
+// app.get("/todos", auth, async function(req, res) {
+//     const userId = req.userId; // Auth middleware se userId ko access kar rahe hain
+
+//     // TodoModel se userId ke basis pe todos ko find kar rahe hain
+//     const todos = await TodoModel.find({
+//         userId: userId
+//     });
+
+//     // Todos ko response mein bhej rahe hain
+//     res.json({
+//         todos: todos
+//     });
+// });
+
+// // Auth middleware: Token verify karne ke liye
+// function auth(req, res, next) {
+//     const token = req.headers.token; // Request headers se token ko nikaal rahe hain
+
+//     try {
+//         // JWT token ko verify kar rahe hain
+//         const decodedData = jwt.verify(token, JWT_SECRET);
+
+//         // Agar token valid hai, toh decoded data se userId ko request object mein set kar rahe hain
+//         req.userId = decodedData.id; // `id` ko token mein se extract kar rahe hain
+//         next(); // Next middleware ya route ko call kar rahe hain
+//     } catch (error) {
+//         // Agar token invalid ho, toh 403 error bhej rahe hain
+//         res.status(403).json({
+//             message: "Incorrect Credentials"
+//         });
+//     }
+// }
+
+// // Server ko 3000 port pe listen karwa rahe hain
+// app.listen(3000);
+
+
+
+//--------------Kirat code----------------
+
+// Import the express, mongoose, and jwt modules
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+// Import the UserModel and TodoModel from the db.js file
+const { UserModel, TodoModel } = require("./db");
+
+// Create an instance of the express module
+const app = express();
+
+// Parse the JSON data using the express.json() middleware
+app.use(express.json());
+
+// Connect to the MongoDB database using the mongoose.connect() method
+mongoose.connect("mongodb+srv://100xdevs:WvaTca0509mb90YX@cluster0.ossjd.mongodb.net/todo-harkirat-2222");
+
+// Create a JWT_SECRET variable for the secret key
+const JWT_SECRET = "hellobacchomajaloclasska";
+
+// Create a POST route for the signup endpoint
+app.post("/signup", async function (req, res) {
+    // Get the email, password, and name from the request body
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    try {
+        // Create a new user using the UserModel.create() method
+        await UserModel.create({
+            email: email,
+            password: password,
+            name: name,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: "User already exists!",
+        });
+    }
+
+    // Send a response to the client
     res.json({
-        message: "You are logged In"
+        message: "You are signed up!",
     });
 });
 
-// Signin route: User ko sign in karne ke liye
-app.post("/signin", async function(req, res) {
-    const { email, password } = req.body; // Email aur password ko request body se nikaal rahe hain
+// Create a POST route for the signin endpoint
+app.post("/signin", async function (req, res) {
+    // Get the email and password from the request body
+    const email = req.body.email;
+    const password = req.body.password;
 
-    // UserModel se user ko database mein search kar rahe hain
+    // Find the user with the given email and password
     const user = await UserModel.findOne({
         email: email,
-        password: password
+        password: password,
     });
 
-    console.log(user); // User ko console pe log kar rahe hain (debugging ke liye)
-
+    // If the user is found, create a JWT token and send it to the client
     if (user) {
-        // Agar user milta hai, toh JWT token generate kar rahe hain
-        const token = jwt.sign({
-            id: user._id.toString() // User ka ID token mein embed kar rahe hain
-        }, JWT_SECRET); // Secret key ke saath token ko sign kar rahe hain
+        // Create a JWT token using the jwt.sign() method
+        const token = jwt.sign(
+            {
+                id: user._id.toString(),
+            },
+            JWT_SECRET
+        );
 
-        // Token ko response mein bhej rahe hain
+        // Send the token to the client
         res.json({
-            token: token
+            token: token,
+            message: "You are signed in!",
         });
     } else {
-        // Agar user nahi milta, toh 403 error bhej rahe hain
+        // If the user is not found, send an error message to the client
         res.status(403).json({
-            message: "Incorrect Credentials"
+            message: "Invalid Credentials!",
         });
     }
 });
 
-// Todo create karne ka route (protected route)
-app.post("/todo", auth, function(req, res) {
-    const userId = req.userId; // Auth middleware se userId ko access kar rahe hain
-    const title = req.body.title; // Todo ka title request body se le rahe hain
-
-    // TodoModel mein naya todo create kar rahe hain
-    TodoModel.create({
-        title: title,
-        userId: userId
-    });
-
-    // Response mein userId bhej rahe hain
-    res.json({
-        userId: userId
-    });
-});
-
-// Todos ko fetch karne ka route (protected route)
-app.get("/todos", auth, async function(req, res) {
-    const userId = req.userId; // Auth middleware se userId ko access kar rahe hain
-
-    // TodoModel se userId ke basis pe todos ko find kar rahe hain
-    const todos = await TodoModel.find({
-        userId: userId
-    });
-
-    // Todos ko response mein bhej rahe hain
-    res.json({
-        todos: todos
-    });
-});
-
-// Auth middleware: Token verify karne ke liye
+// Create an auth middleware function to authenticate the user
 function auth(req, res, next) {
-    const token = req.headers.token; // Request headers se token ko nikaal rahe hain
+    // Get the token from the request headers
+    const token = req.headers.authorization;
 
-    try {
-        // JWT token ko verify kar rahe hain
-        const decodedData = jwt.verify(token, JWT_SECRET);
+    // Verify the token using the jwt.verify() method
+    const decodedData = jwt.verify(token, JWT_SECRET);
 
-        // Agar token valid hai, toh decoded data se userId ko request object mein set kar rahe hain
-        req.userId = decodedData.id; // `id` ko token mein se extract kar rahe hain
-        next(); // Next middleware ya route ko call kar rahe hain
-    } catch (error) {
-        // Agar token invalid ho, toh 403 error bhej rahe hain
+    // If the token is valid, set the userId in the request object and call the next middleware
+    if (decodedData) {
+        // Set the userId in the request object
+        req.userId = decodedData.id;
+
+        // Call the next middleware
+        next();
+    } else {
+        // If the token is invalid, send an error message to the client
         res.status(403).json({
-            message: "Incorrect Credentials"
+            message: "Invalid Token!",
         });
     }
 }
 
-// Server ko 3000 port pe listen karwa rahe hain
+// Create a POST route for the todo endpoint
+app.post("/todo", auth, async function (req, res) {
+    // Get the userId from the request object
+    const userId = req.userId;
+
+    // Get the title, and done from the request body
+    const title = req.body.title;
+    const done = req.body.done;
+
+    // Create a new todo using the TodoModel.create() method
+    await TodoModel.create({
+        userId,
+        title,
+        done,
+    });
+
+    // Send a response to the client
+    res.json({
+        message: "Todo created",
+    });
+});
+
+// Create a GET route for the todo endpoint
+app.get("/todo", auth, async function (req, res) {
+    // Get the userId from the request object
+    const userId = req.userId;
+
+    // Find all the todos with the given userId
+    const todos = await TodoModel.find({
+        userId,
+    });
+
+    // Send the todos to the client
+    res.json({
+        todos,
+    });
+});
+
+// Start the server on port 3000
 app.listen(3000);
