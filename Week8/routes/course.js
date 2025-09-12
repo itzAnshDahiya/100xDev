74c66162174e5b594cf4310537b8252e44d748b8
itzAnshDahiya <anshdahiya00@gmail.com>
2025-09-13 01:04:39 +0530

    Building a Course Selling App

---
+++ Week8/routes/course.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const courseRouter = Router();
+
+
+app.post('/purchase', (req, res) => {
+    res.json({
+        message: "signup  endpoint"
+  })
+});
+
+courseRouter.get('/preview', (req, res) => {
+    res.json({
+        message: "signup endpoint"
+  })
+});
+
+module.exports = {
+    courseRouter: courseRouter
+}
