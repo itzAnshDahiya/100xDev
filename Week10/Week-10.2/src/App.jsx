6f3b98942566f6cf9138935332eca2e56d41685d
itzAnshDahiya <anshdahiya00@gmail.com>
2025-09-18 22:49:51 +0530

    added different types of Code

--- Week10/Week-10.2/src/App.jsx
+++ Week10/Week-10.2/src/App.jsx
@@ -131,3 +131,93 @@ export default App; // App component ko export kar rahe hain taaki baaki app mei
 // }
 
 // export default App; // App component ko export kar rahe hain taaki use kiya ja sake
+
+
+
+
+
+//=======================NOTES============================
+//=====================COUNTER/CLOCK======================
+
+
+// import React, { useEffect, useState } from 'react';
+
+// function Parent() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <>
+//       <Incrase count={count} setCount={setCount} />
+//       <Decrease count={count} setCount={setCount} />
+//       <Value count={count} setCount={setCount} />
+//     </>
+//   );
+// }
+
+// function Decrease({ count, setCount }) {
+//   return <button onClick={() => setCount(count - 1)}>Decrease</button>;
+// }
+
+// function Incrase({ count, setCount }) {
+//   return <button onClick={() => setCount(count + 1)}>Increase</button>;
+// }
+
+// function Value({ count }) {
+//   return <p>Count: {count}</p>;
+// }
+
+// // App Component
+// const App = () => {
+//   return <div>
+//     <Parent />
+//   </div>
+// };
+
+// export default App;
+
+
+
+
+
+
+//==================LIGHT BULB HARKIRAT CODE==================
+// import { useState } from 'react'
+// import './App.css'
+
+// function App() {
+//   return <div>
+//     <LightBulb />
+//   </div>
+// }
+
+// function LightBulb() {
+//   const [bulbOn, setBulbOn] = useState(true)
+
+//   return <div>
+//     <BulbState bulbOn={bulbOn} />
+//     <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+//   </div>
+// }
+
+// function BulbState({bulbOn}) {
+//   return <div>
+//     {bulbOn ? "Bulb on" : "Bulb off"}
+//   </div>
+// }
+
+// function ToggleBulbState({bulbOn, setBulbOn}) {
+
+//   function toggle() {
+//     // setBulbOn(currentState => !currentState)
+//     setBulbOn(!bulbOn)
+    
+//   }
+
+//   return <div>
+//     <button onClick={toggle}>Toggle the bulb</button>
+//   </div>
+// }
+
+// export default App
+
+
