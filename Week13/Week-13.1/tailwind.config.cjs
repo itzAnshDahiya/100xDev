c46cbf0486cfac41b25e2a99c2ba83136b7f0cf3
itzAnshDahiya <anshdahiya00@gmail.com>
2025-10-09 23:40:24 +0530

    added some content

--- Week13/Week-13.1/tailwind.config.cjs
+++ Week13/Week-13.1/tailwind.config.cjs
@@ -1,10 +1,17 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          500: "blue"
+        }
+      }
+    },
   },
   plugins: [],
 }
