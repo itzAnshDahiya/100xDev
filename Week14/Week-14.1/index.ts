5dd2eba75130b057af5eac109497636247c0e7fd
itzAnshDahiya <anshdahiya00@gmail.com>
2026-02-15 00:16:23 +0530

    added new code

---
+++ Week14/Week-14.1/index.ts
@@ -0,0 +1,22 @@
+let x: number = 1;  // type inferencing
+console.log(x);
+
+
+// Add 2 numbers
+// function sum (a: number , b: number){
+//     return a + b;
+// }
+
+// let ans = sum (1,2);
+// console.log(ans);
+
+
+// Return true or false based on if user is 18+ or not
+// function isLegal(age: number){
+//     if(age > 18){
+//         return true;
+//     }else{
+//         false;
+//     }
+// }
+// console.log(isLegal(5));
