9dbf8f1104bc74c49455b0a1e87e34e2d359f8a3
itzAnshDahiya <anshdahiya00@gmail.com>
2026-03-19 00:29:14 +0530

    Successfully Completed Week18

---
+++ Week18/Week-18.2/app/blogs/page.tsx
@@ -0,0 +1,35 @@
+
+import axios from "axios";
+
+async function getBlogs() {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos/")
+    return response.data;
+}
+
+export default async function Blogs() {
+
+    const blogs = await getBlogs();
+
+    return <div>
+        {blogs.map((blog: ITodo) => (
+            <Todo key={blog.id} title={blog.title} completed={blog.completed} />
+        ))}
+    </div>
+}
+
+interface ITodo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+interface TodoProps {
+    title: string;
+    completed: boolean;
+}
+
+function Todo({title, completed}: TodoProps) {
+    return <div>
+        {title} {completed ? "done!" : "not done"}
+    </div>
+}
