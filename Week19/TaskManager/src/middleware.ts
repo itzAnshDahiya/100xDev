4192b6d3b400c8c4497d62b9e2f41a7223ea9526
itzAnshDahiya <anshdahiya00@gmail.com>
2026-04-17 23:59:14 +0530

    added new code

--- Week19/TaskManager/src/middleware.ts
+++ Week19/TaskManager/src/middleware.ts
@@ -41,11 +41,3 @@ export const config = {
     "/((?!api|_next/static|_next/image|favicon.ico|public).*)",
   ],
 };
-
-
-  return res
-}
-
-export const config = {
-  matcher: ['/dashboard/:path*']
-}
