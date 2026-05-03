4b87a541ccc40999c1575fb179de76cf5f333116
itzAnshDahiya <anshdahiya00@gmail.com>
2026-05-03 22:40:48 +0530

    Completed

--- Week2/Week-2.2/index-Calllback_hell.js
+++ Week2/Week-2.2/index-Calllback_hell.js
@@ -1,21 +1,34 @@
-//1. log 'Hi' after 1 second
-//2. log 'hello' 3 second after step 1
-//3. log 'hello there' 5 second after step 2
+// Task:
+// 1. 1 second baad 'Hi' print karna
+// 2. phir 3 second baad 'hello' print karna
+// 3. phir 5 second baad 'hello there' print karna
 
-//The function/Code looks very bad.
+// Yeh code bohot zyada nested hai - callback hell ke roop mein likha hai (bahut bekar pattern)
 
+// Ek simple callback function
 function callback(){
+    // Hi print karti hai
     console.log("Hi");
 }
 
+// Pehla setTimeout - 1 second baad execute hoga
 setTimeout(function(){
+    // Hi print hota hai
     console.log("hi");
+    // Doosra setTimeout nested - 3 second baad execute hoga
     setTimeout(function(){
+        // Hello print hota hai
         console.log("Hello");
+        // Teesra setTimeout nested - 5 second baad execute hoga
         setTimeout(function() {
+        // Hi There print hota hai
         console.log("Hi There");
+        // 5 second ka timeout
         }, 5000 );
+    // 3 second ka timeout
     } , 3000 );
+// 1 second ka timeout
 } , 1000 );
 
+// Yeh line turant print hota hai - setTimeout se pehle (asynchronous)
 console.log("Outside the callback Hell");
--- Week2/Week-2.2/index.js
+++ Week2/Week-2.2/index.js
@@ -1,36 +1,55 @@
+// Rectangle naam ka class bana rahe ho - jismein width, height, aur color ke properties hain
 class Rectangle{
+    // Constructor jo object banate waqt call hota hai - teeno parameters leta hai
     constructor (width, height , color){
+        // Width ko store karti hai
         this.width = width,
+        // Height ko store karti hai
         this.height = height , 
+        // Color ko store karti hai
         this.color = color;
     }
+    // Area nikalne ka method - width aur height ko multiply karta hai
     area(){
+        // Width * Height = Area (area nikaal rahe ho)
         const area = this.width * this.height;
+        // Area return kar rahe ho
         return area;
     }
+    // Paint method - jo color print karti hai
     paint(){    
+        // Console mein color print ho rahe ho
         console.log("Painting with Color " + this.color);  
     }
 }
 
-//create an object = const
-// to create a class to give it values we use = class
-
+// Rectangle ke through object bante hain - naye rectangle ko instantiate kar rahe ho
 const rect = new Rectangle(2,4, "black");
+// rect ke area method ko call kar rahe ho
 const area = rect.area();   
+// rect ko paint karti ho - black color se
 rect.paint();  
+// Area print karti ho
 console.log(area);
+// Doosra rectangle banate ho - size 10x40 blue color ka
 const rect2 = new Rectangle(10,40, "blue");
+// rect2 ka area nikaal rahe ho
 const area1 = rect2.area();
+// rect2 ko paint karti ho
 rect2.paint();  
+// Area print karti ho
 console.log(area1);
 
 
-//old syntax - no class name is attached to it ,  only a simple key value pair.
+// Purana tarika - jab classes nahi the - simple object use karte the
 let rect1 = {
+    // Width property
     width1: 20,
+    // Height property
     height1: 40,
+    // Area nikalne ka function
     area: function(){
+        // Width aur height ko multiply karta hai
         return rect1.width1 * rect1.height1;
     }
 }
