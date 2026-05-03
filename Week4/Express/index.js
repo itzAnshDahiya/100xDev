4a955db6fbc984efc8dc3075feb726f72c2d5f7b
itzAnshDahiya <anshdahiya00@gmail.com>
2026-05-03 22:41:54 +0530

    Completed

--- Week4/Express/index.js
+++ Week4/Express/index.js
@@ -1,29 +1,34 @@
-// const express = require('express')
+// const express = require('express') // Express module ko import (commented)
 
-// function calculateSum(n){
-//     let sum = 0;
-//     for(let i = 1 ; i <=n ; i++){
-//         ans = ans + 1;
+// function calculateSum(n){ // Sum nikalne wala function
+//     let sum = 0; // Initial sum 0
+//     for(let i = 1 ; i <=n ; i++){ // Loop - 1 se n tak
+//         ans = ans + 1; // Increment kar rahe ho
 //     }
-//     return ans;
+//     return ans; // Sum return kar rahe ho
 // }
 
-// const app = express();
+// const app = express(); // Express app banate ho (commented)
 
-// app.get("/" , function(req , res){
-//     const n = req.query.n;
-//     const ans = calculateSum(n);
-//     res.send(ans.toString());
+// app.get("/" , function(req , res){ // GET request handler (commented)
+//     const n = req.query.n; // URL se 'n' parameter lena (commented)
+//     const ans = calculateSum(n); // Sum calculate kar rahe ho (commented)
+//     res.send(ans.toString()); // Response bhejte ho (commented)
 // })
 
-// app.listen(3000);
+// app.listen(3000); // Port 3000 pe listen (commented)
 
+// Express module ko import kar rahe ho
 const express = require("express")
 
+// Express app banate ho
 const app = express();
 
+// Root path (/) pe GET request aaye to yeh handler call hoga
 app.get("/" , function(req , res){
+    // "Hi There" message bhej rahe ho
     res.send("Hi There");
 })
 
+// Server port 3000 pe chalane lag jaata hai
 app.listen(3000);
--- Week4/Express/vegetables.js
+++ Week4/Express/vegetables.js
@@ -1,52 +1,80 @@
+// Express module ko import kar rahe ho
 const express = require('express');
+// Express app banate ho
 const app = express();
+// Port number
 const port = 3000;
 
+// Good fruits ka count - initially 0
 let goodFruitsCount = 0;
+// Bad fruits ka count - initially 0
 let badFruitsCount = 0;
 
+// Express ko JSON data handle karne ke liye kahte ho
 app.use(express.json());
 
+// GET endpoint - fruit counts dekho
 app.get('/fruit-counts', (req, res) => {
+  // JSON response bhej rahe ho
   res.json({
+    // Bad fruits count
     badFruits: badFruitsCount,
+    // Good fruits count
     goodFruits: goodFruitsCount,
   });
 });
 
+// POST endpoint - good fruit count badha do
 app.post('/increase-good', (req, res) => {
+  // Good count ko increment kar rahe ho
   goodFruitsCount++;
+  // Response bhej rahe ho
   res.json({ message: 'Good Fruit count increased!', goodFruits: goodFruitsCount });
 });
 
+// POST endpoint - good fruit count gha do
 app.post('/decrease-good', (req, res) => {
+  // Agar count 0 se zyada hai to
   if (goodFruitsCount > 0) {
+    // Count ko decrement kar rahe ho
     goodFruitsCount--;
+    // Response bhej rahe ho
     res.json({ message: 'Good fruit count decreased!', goodFruits: goodFruitsCount });
   } else {
+    // Agar count already 0 hai to error response
     res.status(400).json({ message: 'Cannot decrease, the count is already 0!' });
   }
 });
 
+// POST endpoint - bad fruit count badha do
 app.post('/increase-bad', (req, res) => {
+  // Bad count ko increment kar rahe ho
   badFruitsCount++;
+  // Response bhej rahe ho
   res.json({ message: 'Bad fruit count increased!', badFruits: badFruitsCount });
 });
 
+// POST endpoint - bad fruit count gha do
 app.post('/decrease-bad', (req, res) => {
+  // Agar count 0 se zyada hai to
   if (badFruitsCount > 0) {
+    // Count ko decrement kar rahe ho
     badFruitsCount--;
+    // Response bhej rahe ho
     res.json({ message: 'Bad fruit count decreased!', badFruits: badFruitsCount });
   } else {
+    // Agar count already 0 hai to error response
     res.status(400).json({ message: 'Cannot decrease, the count is already 0!' });
   }
 });
 
+// Server ko port pe listen karte ho
 app.listen(port, () => {
+  // Server start hone ka message
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// vegetable
+// Vegetable section (vegetables ke liye comments)
 
 //vegetable
 
